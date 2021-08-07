6ec1488cbc881d0c0c8df2f535c91b32d379448d
Jorge Luis <jorgeluisroman1999@gmail.com>
2021-08-07 17:22:04 -0500

    Agregando Register desde la rama cherry

--- IdentityManager/Comparaciones _Detalle_Venta.cs
+++ IdentityManager/Comparaciones _Detalle_Venta.cs
@@ -3,3 +3,13 @@ ventas por hora.
 ventas por quincena
 ventas por mes por mes
 ventas por año por año.
+
+
+   public async Task<IActionResult> Register(string returnurl=null)
+        {
+            if(!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                //create roles
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                await _roleManager.CreateAsync(new IdentityRole("User"));
+            }
