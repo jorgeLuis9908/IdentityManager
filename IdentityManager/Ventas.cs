87256e452de7d19b4ac1ddb858f318e8afb20788
Jorge Luis <jorgeluisroman1999@gmail.com>
2021-08-07 13:03:58 -0500

    Aprendiendo diferencias

--- IdentityManager/Ventas.cs
+++ IdentityManager/Ventas.cs
@@ -2,6 +2,11 @@ agregando linea desde repo local
 Guardando archivo
 FORZAR
 Vamos a forzar el PUSH
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+usiing(importante)
 
 
